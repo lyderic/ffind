52b506e1e5bf39b1184781b47fd49a3c55be4c28
Lyderic Landry <lyderic@lyderic.com>
2017-11-14 10:16:16 +0000

    initial upload

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func main() {
+
+	hiddenPtr := flag.Bool("H", false, "show hidden files/dirs")
+	removePtr := flag.Bool("Hr", false, "remove hidden files/dirs")
+	flag.Parse()
+	hidden := *hiddenPtr
+	remove := *removePtr
+
+	basedir := "."
+	if flag.NArg() > 0 {
+		basedir = os.Args[len(os.Args)-1]
+	}
+	if !exists(basedir) {
+		log.Fatalln(basedir, "not found!")
+	}
+
+	if hidden {
+		listHidden(basedir)
+		return
+	}
+
+	if remove {
+		listHidden(basedir)
+		removeHidden(basedir)
+		return
+	}
+
+	/* This is the default, if no flag is used */
+	listAll(basedir)
+
+}
+
+func listAll(basedir string) {
+	filepath.Walk(basedir, func(path string, info os.FileInfo, err error) error {
+		display(path, info)
+		return err
+	})
+}
+
+func listHidden(basedir string) {
+	filepath.Walk(basedir, func(path string, info os.FileInfo, err error) error {
+		if info.Name()[0] == '.' && len(info.Name()) > 1 {
+			display(path, info)
+		}
+		return err
+	})
+}
+
+func removeHidden(basedir string) {
+	filepath.Walk(basedir, func(path string, info os.FileInfo, err error) error {
+		if info.Name()[0] == '.' && len(info.Name()) > 1 {
+			err = os.RemoveAll(path)
+		}
+		return err
+	})
+}
+
+func exists(path string) (found bool) {
+	found = true
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		found = false
+	}
+	return
+}
+
+func display(path string, info os.FileInfo) {
+  if info.IsDir() {
+    path = path + "/"
+  }
+  fmt.Println(path)
+}
